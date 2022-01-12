c178331166d6e19fce133b35b947a4c0366393fe
Florian Emile <emile@galatea-associates.com>
2022-01-12 19:33:19 +0000

    Add discount method in checkout rule

--- src/main/java/kata/supermarket/checkout/Checkout.java
+++ src/main/java/kata/supermarket/checkout/Checkout.java
@@ -5,6 +5,8 @@ import kata.supermarket.discount.DiscountManager;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Checkout {
 
@@ -17,13 +19,30 @@ public class Checkout {
   }
 
   public BigDecimal getCost() {
-    return subtotal();
+    return subtotal().subtract(discounts());
   }
 
+  // TODO: merge sub total and discount together
   private BigDecimal subtotal() {
     return basket.items().stream().map(Item::price)
         .reduce(BigDecimal::add)
         .orElse(BigDecimal.ZERO)
         .setScale(2, RoundingMode.HALF_UP);
+  }
+
+  private BigDecimal discounts() {
+    Map<String, Integer> groupedItems = new HashMap<>();
+    basket.items().forEach(i -> {
+      int count = groupedItems.getOrDefault(i.getId(), 0) + 1;
+      groupedItems.put(i.getId(), count);
+    });
+
+    BigDecimal newPrice = BigDecimal.ZERO;
+    for (Map.Entry<String, Integer> entry : groupedItems.entrySet()) {
+      String id = entry.getKey();
+      Integer count = entry.getValue();
+      newPrice = newPrice.add(discountManager.applyDiscount(id, count));
+    }
+    return newPrice;
   }
 }
